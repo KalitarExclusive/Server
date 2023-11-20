0bc2e61345f2a5512bcedaa60c808131a4acdaa4
KalitarExclusive <71302169+KalitarExclusive@users.noreply.github.com>
2023-11-19 19:56:07 -0800

    nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Update package list and install Nginx
+sudo apt update
+sudo apt install nginx -y
+
+# Start and enable Nginx service
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+# Define your domain names and corresponding port numbers
+declare -A domains=( ["example1.com"]="3001" ["example2.com"]="3002" ["example3.com"]="3003" )
+
+# Create Nginx server blocks for each domain
+for domain in "${!domains[@]}"; do
+    config="/etc/nginx/sites-available/$domain"
+    port=${domains[$domain]}
+
+    # Create the server block configuration
+    echo "Creating Nginx server block for $domain on port $port"
+    cat <<EOF | sudo tee $config
+server {
+    listen 80;
+    server_name $domain;
+
+    location / {
+        proxy_pass http://localhost:$port;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF
+
+    # Enable the configuration
+    sudo ln -s $config /etc/nginx/sites-enabled/
+done
+
+# Test Nginx configuration and restart the service
+sudo nginx -t && sudo systemctl restart nginx
+
+echo "Nginx configuration complete. If there were no errors, your reverse proxy is now set up."
