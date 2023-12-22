f48ac830922d56cf09bef19bbb280194408f8222
KalitarExclusive <71302169+KalitarExclusive@users.noreply.github.com>
2023-12-21 23:31:48 -0800

    Bitwarden.sh

--- Bitwarden.sh
+++ Bitwarden.sh
@@ -20,8 +20,22 @@ sudo apt install docker-compose -y
 curl -Lso bitwarden.sh https://go.btwrdn.co/bw-sh
 chmod +x bitwarden.sh
 
+# Install Certbot for Let's Encrypt
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
 # Run Bitwarden setup
 ./bitwarden.sh install
 
+# Obtain SSL certificate
+echo "Please enter your domain name (e.g., bitwarden.example.com): "
+read DOMAIN_NAME
+sudo certbot certonly --standalone -d $DOMAIN_NAME
+
+# Configure Bitwarden to use the SSL certificate
+sudo ./bitwarden.sh renewssl $DOMAIN_NAME
+
 # Start Bitwarden
 ./bitwarden.sh start
+
